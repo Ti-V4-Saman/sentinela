be6afafaa7bf6d20edaeb7096bf78741cb95eb3a
GiovaniMaia <giovani.maia2011@gmail.com>
2026-07-23 09:44:53 -0300

    refactor: use .env file for database credentials

--- server/db.js
+++ server/db.js
@@ -9,11 +9,11 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '143.244.156.195',
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'V4SA@2210!',
-  database: process.env.DB_NAME || 'sentinela',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
